f6856af350976fafa8287f92e9844528996aaa79
Fábio Linhares <fabiolinharez@gmail.com>
2025-01-28 20:26:38 -0300

    corrige caminho de importação do componente App em index.js

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";  // O caminho do seu componente principal
+import App from ".docs/App"; 
 
 ReactDOM.render(<App />, document.getElementById("root"));
